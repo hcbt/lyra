19e75426a7042483aa6341c069f82f86ff147070
hcbt <hcbt@protonmail.com>
2020-10-24 18:29:51 +0300

    Removed spotify api key

--- lyra/spotify/client.py
+++ lyra/spotify/client.py
@@ -5,7 +5,7 @@ from spotipy.oauth2 import SpotifyClientCredentials
 def main():
     #redirect_uri = "", scope = "user-library-read"
     sp = spotipy.Spotify(auth_manager = SpotifyClientCredentials(client_id = "0aa44a85743746e899db22ea4bfc94c6", 
-                                                                 client_secret = "616e94f39eff4f94a1648a9c77125220"))
+                                                                 client_secret = ""))
 
     results = sp.search(q = "Plastikman - Spastik", limit = 20)
 
